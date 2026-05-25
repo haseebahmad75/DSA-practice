e4f71fc2cae7fafef5d916db915b97c51be90726
Haseeb Ahmad <haseebahmaduet4@gmail.com>
2026-05-25 13:55:44 -0700

    Check palindrome string

---
+++ Stack/check_palindrome.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<stack>
+#include<cstring>
+using namespace std;
+
+// Palindrome string means if you reverse the string, it stays unchanged.
+
+string ReverseString(string s)
+{
+    stack<char> S;
+    string rev;
+
+    for(int  i = 0; i < s.length(); i++)
+    {
+        S.push(s[i]);
+    }
+
+    for(int i = 0; i < s.length(); i++)
+    {
+        rev += S.top();
+        S.pop();
+    }
+    return rev;
+}
+
+// for string, use length() and getline(cin, string_name) to take input
+// for char[] (array of characters), use strlen(name) and cin.getline(name, size)
+
+int main()
+{
+    string s;
+    cout<<"Enter a word: ";
+    getline(cin,s);
+
+    string reversed = ReverseString(s);
+    if(s == reversed)
+    {
+        cout<<"Palindrome";
+    }
+    else
+    {
+        cout<<"Not Palindrome";
+    }
+}
